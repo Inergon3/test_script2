907daca98a3e4e4797297ae055f174ae728207d9
inergon3 <mr.morala@mail.ru>
2025-05-10 14:13:57 +0700

    add typing

--- file.py
+++ file.py
@@ -1,31 +1,32 @@
 import json
 import os
 from collections import defaultdict
+from typing import List, Union, Dict
 
 from worker import Worker
 
 
 class File:
     def __init__(self, arg):
-        self.files: list = arg
-        self.read_files: list = []
+        self.files: List[str] = arg
 
-    def read(self):
+    def read(self) -> List[List[List[str]]]:
+        read_files: List[List[List[str]]] = []
         for file in self.files:
             try:
                 with open(file, "r") as f:
-                    lines: list[str] = f.readlines()
-                    read_file: list[str] = []
+                    lines: List[str] = f.readlines()
+                    read_file: List[str] = []
                     for line in lines:
                         read_file.append(line.replace("\n", "").split(","))
-                    self.read_files.append(read_file)
+                    read_files.append(read_file)
             except:
                 raise ValueError(f"file {file} not found")
-        return self.read_files
+        return read_files
 
-    def convert_list_to_dict(self):
-        read_file = self.read()
-        file_list_dict: list = []
+    def convert_list_to_dict(self) -> List[Dict[str, str]]:
+        read_file: List[List[List[str]]] = self.read()
+        file_list_dict: List[List[str]] = []
         for file in read_file:
             header = file[0]
             file.pop(0)
@@ -40,18 +41,18 @@ class File:
                 file_list_dict.append(cache)
         return file_list_dict
 
-    def create_list_worker(self):
-        list_dicts = self.convert_list_to_dict()
-        list_obj = []
+    def create_list_worker(self) -> List[Worker]:
+        list_dicts: List[Dict[str, str]] = self.convert_list_to_dict()
+        list_obj: List[Worker] = []
         for dict in list_dicts:
             worker = Worker(id=dict["id"], email=dict["email"], name=dict["name"], department=dict["department"],
                             hours=dict["hours_worked"], rate=dict["rate"])
             list_obj.append(worker)
         return list_obj
 
-    def create_report_payout(self, filename):
-        list_obj = self.create_list_worker()
-        dict_dep = defaultdict(lambda: {
+    def create_report_payout(self, filename) -> Dict[str, Dict[str, Union[int, List[Dict[str, Union[str, int]]]]]]:
+        list_obj: List[Worker] = self.create_list_worker()
+        dict_dep: Dict[str, Dict[str, Union[int, List[Dict[str, Union[str, int]]]]]] = defaultdict(lambda: {
             "workers": [],
             "total_hours": 0,
             "total_payout": 0
@@ -73,14 +74,14 @@ class File:
         self.print_report(dict_dep)
         return dict_dep
 
-    def save_report(self, data, filename):
+    def save_report(self, data, filename) -> None:
         try:
             with open(filename, "w") as f:
                 json.dump(data, f)
         except:
             os.makedirs(os.path.dirname(filename))
 
-    def print_report(self, report):
+    def print_report(self, report) -> None:
         print(15 * " ", "name", 11 * " ", "hours", 2 * " ", "rate", 1 * " ", "payout", 9 * " ")
         for dep, data in sorted(report.items()):
             print(dep)
@@ -92,4 +93,3 @@ class File:
                       ((15 - len(str(payout))) * " "))
             print(32 * " ", data["total_hours"], (7 - len(str(data["total_hours"]))) * " ", 6 * " ", "$",
                   data["total_payout"])
-
--- worker.py
+++ worker.py
@@ -1,9 +1,9 @@
 class Worker:
-    def __init__(self, id, email, name, department, hours, rate):
-        self.id = int(id)
-        self.email = email
-        self.name = name
-        self.department = department
-        self.hours = int(hours)
-        self.rate = int(rate)
-        self.payout = self.hours * self.rate
+    def __init__(self, id: str, email: str, name: str, department: str, hours: str, rate: str):
+        self.id: int = int(id)
+        self.email: str = email
+        self.name: str = name
+        self.department: str = department
+        self.hours: int = int(hours)
+        self.rate: int = int(rate)
+        self.payout: int = self.hours * self.rate
